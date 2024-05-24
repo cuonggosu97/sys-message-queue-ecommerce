88783d587b4107ab564a0810e3aa6d622fea7c27
Cường Phạm <>
2024-05-24 11:28:34 +0700

    xử lý message khi gặp sự cố TTL và LOGIC

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 "use strict";
 
-const { consumerToQueue } = require("./src/services/consumerQueue.service");
+const {
+  consumerToQueue,
+  consumerToQueueNormal,
+  consumerToQueueFail,
+} = require("./src/services/consumerQueue.service");
 const queueName = "test-topic";
-consumerToQueue(queueName)
+
+// consumerToQueue(queueName)
+//   .then(() => {
+//     console.log(`Message consumer started ${queueName}`);
+//   })
+//   .catch((error) => {
+//     console.error(`Message Error: ${error.message}`);
+//   });
+
+consumerToQueueNormal(queueName)
   .then(() => {
-    console.log(`Message consumer started ${queueName}`);
+    console.log(`Message consumerToQueueNormal started`);
+  })
+  .catch((error) => {
+    console.error(`Message Error: ${error.message}`);
+  });
+
+consumerToQueueFail(queueName)
+  .then(() => {
+    console.log(`Message consumerToQueueFail started`);
   })
   .catch((error) => {
     console.error(`Message Error: ${error.message}`);
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -11,6 +11,81 @@ const messageService = {
       console.error("Error in consumerToQueue", error);
     }
   },
+
+  // case processing
+  consumerToQueueNormal: async (queueName) => {
+    try {
+      const { channel } = await connectToRabbitMQ();
+      const notiQueue = "notificationQueueProcess"; // assertQueue
+      // 1. TTL
+      // const timeExpire = 15000;
+      // setTimeout(async () => {
+      //   await channel.consume(notiQueue, (msg) => {
+      //     console.log(
+      //       `SEND notificationQueue successfully processed:  ${msg.content.toString()}`
+      //     );
+      //     channel.ack(msg);
+      //   });
+      // }, timeExpire);
+
+      // 2. LOGIC
+      await channel.consume(notiQueue, (msg) => {
+        try {
+          const numberTest = Math.random();
+          console.log({ numberTest });
+          if (numberTest < 0.5) {
+            throw new Error("Send notification failed: HOT FIX");
+          }
+          console.log(
+            `SEND notificationQueue successfully processed:  ${msg.content.toString()}`
+          );
+          channel.ack(msg);
+        } catch (error) {
+          channel.nack(msg, false, false);
+        }
+      });
+    } catch (error) {
+      console.error("Error in consumerToQueueNormal", error);
+    }
+  },
+
+  // case failed processing
+  consumerToQueueFail: async (queueName) => {
+    try {
+      const { channel } = await connectToRabbitMQ();
+      const notificationExchangeDLX = "notificationExDLX"; // notificationExDLX direct
+      const notificationRoutingKeyDLX = "notificationRoutingKeyDLX"; // assert
+      const notiQueueHandler = "notificationQueueHotFix";
+
+      await channel.assertExchange(notificationExchangeDLX, "direct", {
+        durable: true,
+      });
+
+      const queueResult = await channel.assertQueue(notiQueueHandler, {
+        exclusive: false,
+      });
+
+      await channel.bindQueue(
+        queueResult.queue,
+        notificationExchangeDLX,
+        notificationRoutingKeyDLX
+      );
+      await channel.consume(
+        queueResult.queue,
+        (msgFailed) => {
+          console.log(
+            `this notification error: pls hot fix:: ${msgFailed.content.toString()}`
+          );
+        },
+        {
+          noAck: true,
+        }
+      );
+    } catch (error) {
+      console.error("Error in consumerToQueueFail", error);
+      throw error;
+    }
+  },
 };
 
 module.exports = messageService;
